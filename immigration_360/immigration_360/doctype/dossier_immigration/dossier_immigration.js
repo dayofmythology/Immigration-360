dfb7f3f76a8d385a536d474cbb4ab3a913460d3f
dayofmythology <dayofmythology@gmail.com>
2021-05-01 18:07:57 -0700

    +dossier immigration doctype

---
+++ immigration_360/immigration_360/doctype/dossier_immigration/dossier_immigration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Media Desk SARL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Dossier Immigration', {
+	// refresh: function(frm) {
+
+	// }
+});
